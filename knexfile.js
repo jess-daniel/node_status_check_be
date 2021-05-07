a708ff5152cb4c924cf570946800abc805672694
jess-daniel <jessdaniel474@gmail.com>
2021-05-07 00:16:23 -0400

    adds ssl to knex config for heroku pg

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: '127.0.0.1',
-      database: 'dev_status_check',
+      host: "127.0.0.1",
+      database: "dev_status_check",
       user: process.env.PG_USER,
       port: process.env.PG_PORT,
       password: process.env.PG_PASSWORD,
@@ -15,25 +15,30 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
   },
 };
